548f3c079af41e1ceed98642cb5294a26aec7de9
Ben Beckerman <benjamin.beckerman@crowdflower.com>
2017-01-26 18:08:15 -0800

    add scss to webpack config

--- client/app/components/pages/ListingsPage/ListingsPage.jsx
+++ client/app/components/pages/ListingsPage/ListingsPage.jsx
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { observer } from 'mobx-react';
 
+import './ListingsPage.scss';
 import ListingsState from './ListingsState.js';
-
 
 @observer export default class ListingsPage extends Component {
   static propTypes = {
@@ -14,9 +14,9 @@ import ListingsState from './ListingsState.js';
 
   render() {
     return (
-      <div>
+      <div className="ListingsPage">
         <h2>This is the listings index page </h2>
-        <p>
+        <p className="ListingsPage__description">
           This is rendered from mobx state: {this.props.listingsState.get('hello')}
         </p>
       </div>
